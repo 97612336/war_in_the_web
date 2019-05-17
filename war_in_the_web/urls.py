9254864ed80ab4a0bc39fae18ef26f2f4eef9f2f
xkjskk@163.com <xkjskk@163.com>
2019-05-17 15:50:37 +0800

    增加新的架构

--- war_in_the_web/urls.py
+++ war_in_the_web/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from army.urls import army_urls
+from city.urls import city_urls
 
 urlpatterns = [
+    path('city/', include(city_urls)),
+    path('army/', include(army_urls)),
     path('admin/', admin.site.urls),
 ]
